04e6d669d433e5fb872b652bf02196f05355b59b
Matto58 <thematto58@gmail.com>
2025-06-10 18:19:44 +0200

    build with git sdl_ttf

--- build.sh
+++ build.sh
@@ -4,7 +4,15 @@ echo "------ KITCODE: building lua"
 cd lua
 make all
 )
+echo "------ KITCODE: building SDL_ttf"
+(
+cd SDL_ttf
+cmake -S . -B build
+cd build
+make
+)
 echo "------ KITCODE: building kitcode"
-g++ $(pkg-config --cflags --libs sdl3 sdl3-ttf gtk+-3.0) -o kitcode $(find src/*.cpp) \
-    -Ilua/src -Llua/src -llua
+g++ $(pkg-config --cflags --libs sdl3) -o kitcode $(find src/*.cpp) \
+    -Ilua/src -Llua/src -llua \
+    -ISDL_ttf/include -LSDL_ttf/build -lSDL3_ttf
 exit $?
